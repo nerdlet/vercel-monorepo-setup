ac8cecd98a02aca567b57b5e9ff10f0d1c390bc1
nerdlet <brndnyokabi@gmail.com>
2021-01-29 11:55:14 +0300

    Update packages

--- apps/my-app/src/App.js
+++ apps/my-app/src/App.js
@@ -1,12 +1,12 @@
 
 import React from 'react'
-import {Button} from '@vercel-monorepo-setup/ui'
+//import {Button} from '@vercel-monorepo-setup/ui'
 
 function App() {
   return (
     <div >
       <div>this is an example project</div>
-			<Button>this is a button</Button>
+			{/* <Button>this is a button</Button> */}
     </div>
   );
 }
