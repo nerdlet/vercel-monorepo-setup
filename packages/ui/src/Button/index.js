d5edc2ba78457ec7133fd8372d22a1d2ff92bae1
nerdlet <brndnyokabi@gmail.com>
2021-01-29 13:03:47 +0300

    Remove public folder

---
+++ packages/ui/src/Button/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button() {
+  return (
+   <div>
+		 <button>this is a test.</button>
+	 </div>
+  );
+}
+
+export default Button;
--- packages/ui/webpack.config.js
+++
@@ -1,4 +0,0 @@
-const path = require('path');
-const getPackageConfig = require('../../../webpack/get-package-config');
-
-module.exports = getPackageConfig({ base: path.join(__dirname, './') });
